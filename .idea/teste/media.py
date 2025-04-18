69535a7cec77871670c599e8847c614433551158
Edmilson Filho <89650955+edmilsonfilho1@users.noreply.github.com>
2025-04-18 10:03:24 -0300

    Update media.py

--- .idea/teste/media.py
+++ .idea/teste/media.py
@@ -2,3 +2,5 @@ n1 = float(input('Digite um número: '))
 n2 = float(input('Digite outro número: '))
 media = (n1+n2)/2
 print(f'A média entre {n1} e {n2} é igual a {media}')
+
+#testando a adição de informações diretamente pelo site 
